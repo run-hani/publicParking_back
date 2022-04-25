d0cf0a5a9f9b5d4f123249c693017e557f484a4e
run-hani <hanirun@kakao.com>
2022-04-25 22:48:04 +0900

    add: 서버 파일 세팅

--- app/models/index.js
+++ app/models/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const { MONGO_URI } = process.env;
-const mongoose = require('mongoose')
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import UserModel from './User.js'
 mongoose.Promise = global.Promise
 
 const db = {}
 db.mongoose = mongoose
-db.url = MONGO_URI
-db.user = require('./user.model')(mongoose)
+db.url = dotenv.MONGO_URI
+db.User = new UserModel(mongoose)
 
-module.exports= db
+export default db
---
+++ app/services/user.js
@@ -0,0 +1,97 @@
+import db from '../models/index.js'
+import getDatabase from '../lambdas/getDatabase.js'
+
+export default function UserService(){
+    const User = db.User
+    const dbo = getDatabase()
+    const dbConnect = dbo.getDb();
+
+    return {
+        join(req, res){
+            console.log(' ### 6. join 진입 ###' + JSON.stringify(req.body))
+            new User(req.body).save(function(err){
+                if(err){
+                    res.status(500).json({message: err})
+                    console.log('회원가입 실패')
+                    return;
+                }else{
+                    console.log(' ### 7. join 성공 ###');
+                    res.status(200).json({ok: 'ok'})
+                }
+            })
+
+            
+        },
+        login(req, res) {
+            User.findOne({
+                userid: req.body.userid
+            }, function (err, user) {
+                if (err) 
+                    throw err
+                if (!user) {
+                    res
+                        .status(401)
+                        .send({success: false, message: '해당 ID가 존재하지 않습니다'});
+                } else {
+                    console.log(' ### 로그인 정보 : ' + JSON.stringify(user))
+                    user.comparePassword(req.body.password, function (_err, isMatch) {
+                        if (!isMatch) {
+                            res
+                                .status(401)
+                                .send({message:'FAIL'});
+                        } else {
+                            user.generateToken((err, user) => {
+                                if (err) 
+                                    res
+                                        .status(400)
+                                        .send(err)
+
+                                    // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+                                res
+                                    .status(200)
+                                    .json(user)
+                            })
+                        }
+                    })
+                }
+            })
+        },
+        logout () {
+            req.logout()
+            res.json({msg: 'LOGOUT'})
+        },
+        checkDuplicateUserid(req, res) {
+            User
+                .findById({userid: req.body.userid})
+                .exec((err, user) => {
+                    if (err) {
+                        res
+                            .status(500)
+                            .send({message: err});
+                        return;
+                    }
+                    if (user) {
+                        res
+                            .status(400)
+                            .send({message: "ID가 이미 존재합니다"});
+                        return;
+                    }
+                })
+        },
+        getUserById(req, res){
+            const userid = req.body.userId;
+            User
+                .findById({userid: userid})
+                .exec((_err, user) => {
+                    return user
+                })
+        },
+        getUsers(req, res) {
+            User.find().exec(
+                (err, users)=>{
+                    res.status(200).json(users)
+                }
+            )
+        }
+    }
+}
