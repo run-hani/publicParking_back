17c4ac30df7b1f70de05732b7df19125be3e9097
run-hani <hanirun@kakao.com>
2022-04-29 21:40:51 +0900

    update: api 경로 변경

--- app/services/board.js
+++ app/services/board.js
@@ -1,52 +1,41 @@
 import db from '../models/index.js'
 import getDatabase from '../lambdas/getDatabase.js'
 
-export default function SacService(){
-
+export default function BoardService() {
   const Board = db.Board;
   const dbo = getDatabase();
 
   return {
-    add(req, res){
-      console.log("SAC regist");
-      new Board(req.body).save(function(err){
-        if(err){
-          res.status(500).json({message:err});
+    add(req, res) {
+      new Board(req.body).save(function (err) {
+        if (err) {
+          res.status(500).json({ message: err });
         } else {
-          res.status(200).json({ok : "ok"});
+          res.status(200).json({ ok: "ok" });
         }
-      })
+      });
     },
-    getPosts(req, res){
-      Board.find().exec(
-        (err, sac) => {
-          res.status(200).json({sac});
+    getPosts(req, res) {
+      Board.find().exec((err, posts) => {
+        res.status(200).json({ posts });
+      });
+    },
+    getPostById(id, res) {
+      Board.findById({ _id: id }).exec((err, post) => {
+        res.status(200).json({ post });
+      });
+    },
+    update(req, res) {
+      Board.updateOne({ _id: req.body._id }, { $set: req.body }).exec(
+        (err, post) => {
+          res.status(200).json({ ok: "ok" });
         }
       );
     },
-    getPostById(id, res){
-      Board.findById({_id : id})
-      .exec(
-        (err, sac) => {
-          res.status(200).json({sac});
-        }
-      )
+    delete(id, res) {
+      Board.deleteOne({ _id: id }).exec((err, post) => {
+        res.status(200).json({ ok: "ok" });
+      });
     },
-    update(req, res){
-      Board.updateOne({_id : req.body._id}, { $set : req.body }).exec(
-        (err, sac) => {
-          res.status(200).json({ok : "ok"});
-        }
-
-      )
-    },
-    delete(id, res){
-      Board.deleteOne({_id : id}).exec(
-        (err, sac) => {
-          res.status(200).json({ok : "ok"});
-        }
-      )
-    }
-  }
-
+  };
 }
